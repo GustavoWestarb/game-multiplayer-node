eba356d90143f3be9e3593bab45950a6b410f121
Gustavo Westarb <gustavowestarb45@gmail.com>
2019-12-20 21:36:36 +0000

    Parte inicial do jogo finalizada

--- 1st-release/server.js
+++ 1st-release/server.js
@@ -10,17 +10,32 @@ const sockets = socketio(server);
 app.use(express.static('public'));
 
 const game = createGame();
+game.start();
 
-// game.addPlayer({ playerId: 'player1', playerX: 0, playerY: 0});
-// game.addFruit({ fruitId: 'fruit1', fruitX: 3, fruitY: 3});
-
-console.log(game.state);
+game.subscribe((command) => {
+    console.log(`> Emitting ${command.type}`);
+    sockets.emit(command.type, command)
+});
 
 sockets.on('connection', (socket) => {
     const playerId = socket.id;
     console.log(`> Player connected on Server with id: ${playerId}`);
 
-    socket.emit('setup', game.state)
+    game.addPlayer({ playerId: playerId});
+
+    socket.emit('setup', game.state);
+
+    socket.on('disconnect', () => {
+        game.removePlayer({ playerId: playerId });
+        console.log(`> Player disconnected: ${playerId}`);
+    });
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId;
+        command.type = 'move-player';
+        
+        game.movePlayer(command);
+    });
 })
 
 server.listen(3000, () => {
